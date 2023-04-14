2c22779c66228d4dd591eeee19aa8bd6b5c35f96
blo0dy66 <andriyprotsiv76@gmail.com>
2023-04-14 16:49:22 +0300

    added 10 task

--- js-hometask-10/js/task5.js
+++ js-hometask-10/js/task5.js
@@ -8,3 +8,9 @@ function mapBuilder(keysArray, valuesArray) {
     }
     return map;
 }
+
+let keys = [1, 2, 3, 4];
+let values = ["div", "span", "b", "i"];
+let map = mapBuilder(keys, values);
+console.log(map.size); // 4
+console.log(map.get(2)); // "span"
